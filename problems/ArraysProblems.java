4de48e590070c694ebd89b801de2619b95866f30
Sahaj Rajput <sahajrajput1112@gmail.com>
2025-12-30 11:02:01 +0530

    find the second largest element in the array problem solved by - brute , better and optimal approach

--- problems/ArraysProblems.java
+++ problems/ArraysProblems.java
@@ -53,9 +53,70 @@ public class ArraysProblems {
 
         return true;
     }
+
+
+    //FIND THE SECOND LARGEST ELEMENT PROBLEM
+    
+    //brute force approach for solving secondLargest elemnt problem
+    public int secondLargestElement(int[] arr){
+        int n=arr.length;
+        Arrays.sort(arr);
+
+        int largest=arr[n-1];
+        int secondLargest=0;
+
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]!=largest){
+                secondLargest=arr[i];
+                
+                break;
+            } 
+        }
+
+        return secondLargest;
+    }
+    //better aproach 
+    public int secondLar(int[] arr){
+        int n=arr.length;
+
+        int largest=arr[0];
+        for(int i=0;i<n;i++){
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        
+        int secondLargest=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>secondLargest && arr[i]!=largest){
+                secondLargest=arr[i];
+            }
+
+        }
+
+        return secondLargest;
+    }
+
+    //optimal approach
+    public int secLargest(int[] arr){
+        int largest=arr[0];
+        int sLargest=-3;
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>largest){
+                sLargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i] < largest && arr[i]>sLargest){
+                sLargest=arr[i];
+            }
+        }
+
+        return sLargest;
+    }
     public static void main(String[] args) {
 
-        int[] arr={3,4,5,6,7};
+        int[] arr={3,4,6,6,6};
         ArraysProblems obj=new ArraysProblems();
 
         obj.reverseArray(arr);
@@ -66,6 +127,12 @@ public class ArraysProblems {
         System.out.println(obj.sumOfElements(arr));
 
         System.out.println(obj.isSorted(arr));
+
+        System.out.println(obj.secondLargestElement(arr));
+
+        System.out.println(obj.secondLar(arr));
+
+        System.out.println(obj.secLargest(arr));
 
         
     }
